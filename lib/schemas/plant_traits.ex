6d6a8fc4cc33868f307b77fd80d8a1a22ae2e543
FTITOR <pac.ko1992@hotmail.com>
2022-01-17 21:07:41 -0600

    changeset added

---
+++ lib/schemas/plant_traits.ex
@@ -0,0 +1,43 @@
+defmodule Chirinola.Schema.PlantTraits do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @fields ~w(LastName FirstName DatasetID Dataset SpeciesName AccSpeciesID AccSpeciesName ObservationID ObsDataID TraitID TraitName DataID DataName OriglName OrigValueStr OrigUnitStr ValueKindName OrigUncertaintyStr UncertaintyName Replicates StdValue UnitName RelUncertaintyPercent OrigObsDataID ErrorRisk Reference Comment NoNameColumn)a
+
+  schema "plant_traits" do
+    field(:LastName, :string)
+    field(:FirstName, :string)
+    field(:DatasetID, :integer)
+    field(:Dataset, :string)
+    field(:SpeciesName, :string)
+    field(:AccSpeciesID, :integer)
+    field(:AccSpeciesName, :string)
+    field(:ObservationID, :integer)
+    field(:ObsDataID, :integer)
+    field(:TraitID, :integer)
+    field(:TraitName, :string)
+    field(:DataID, :integer)
+    field(:DataName, :string)
+    field(:OriglName, :string)
+    field(:OrigValueStr, :string)
+    field(:OrigUnitStr, :string)
+    field(:ValueKindName, :string)
+    field(:OrigUncertaintyStr, :string)
+    field(:UncertaintyName, :string)
+    field(:Replicates, :integer)
+    field(:StdValue, :float)
+    field(:UnitName, :string)
+    field(:RelUncertaintyPercent, :float)
+    field(:OrigObsDataID, :integer)
+    field(:ErrorRisk, :float)
+    field(:Reference, :string)
+    field(:Comment, :string)
+    field(:NoNameColumn, :string)
+    timestamps()
+  end
+
+  def changeset(plant_trait, params) do
+    plant_trait
+    |> cast(params, @fields)
+  end
+end
