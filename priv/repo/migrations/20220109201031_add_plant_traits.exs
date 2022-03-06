9644a96fd6ec3ad61e60051b87a90389bb25d23a
FTITOR <pac.ko1992@hotmail.com>
2022-03-05 19:39:41 -0600

    cols rename

--- priv/repo/migrations/20220109201031_add_plant_traits.exs
+++ priv/repo/migrations/20220109201031_add_plant_traits.exs
@@ -3,34 +3,34 @@ defmodule Chirinola.Repo.Migrations.AddPlantTraits do
 
   def up do
     create table(:plant_traits, primary_key: true) do
-      add :LastName, :string
-      add :FirstName, :string
-      add :DatasetID, :integer
-      add :Dataset, :string
-      add :SpeciesName, :string
-      add :AccSpeciesID, :integer
-      add :AccSpeciesName, :string
-      add :ObservationID, :integer
-      add :ObsDataID, :integer
-      add :TraitID, :integer
-      add :TraitName, :string
-      add :DataID, :integer
-      add :DataName, :string
-      add :OriglName, :string
-      add :OrigValueStr, :string
-      add :OrigUnitStr, :string
-      add :ValueKindName, :string
-      add :OrigUncertaintyStr, :string
-      add :UncertaintyName, :string
-      add :Replicates, :float
-      add :StdValue, :float
-      add :UnitName, :string
-      add :RelUncertaintyPercent, :float
-      add :OrigObsDataID, :integer
-      add :ErrorRisk, :float
-      add :Reference, :text
-      add :Comment, :text
-      add :NoNameColumn, :string
+      add :last_name, :string
+      add :first_name, :string
+      add :dataset_id, :integer
+      add :dataset, :string
+      add :species_name, :string
+      add :acc_species_id, :integer
+      add :acc_species_name, :string
+      add :observation_id, :integer
+      add :obs_data_id, :integer
+      add :trait_id, :integer
+      add :trait_name, :string
+      add :data_id, :integer
+      add :data_name, :string
+      add :origl_name, :string
+      add :orig_value_str, :string
+      add :orig_unit_str, :string
+      add :value_kind_name, :string
+      add :orig_uncertainty_str, :string
+      add :uncertainty_name, :string
+      add :replicates, :float
+      add :std_value, :float
+      add :unit_name, :string
+      add :rel_uncertainty_percent, :float
+      add :orig_obs_data_id, :integer
+      add :error_risk, :float
+      add :reference, :text
+      add :comment, :text
+      add :no_name_column, :string
       timestamps()
     end
   end
