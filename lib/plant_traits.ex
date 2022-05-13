ddb7b0ec2214c5764113499b6c838083a8100d44
FTITOR <pac.ko1992@hotmail.com>
2022-05-13 11:39:29 -0500

    Chirinola.PlantTrait module is added

---
+++ lib/plant_traits.ex
@@ -0,0 +1,45 @@
+defmodule Chirinola.PlantTrait do
+  @moduledoc """
+  Documentation for `PlantTrait` module.
+  """
+
+  require Logger
+  alias Chirinola.Repo
+  alias Chirinola.Schema.PlantTraits, as: PlantTraitsSchema
+
+  @doc """
+
+  """
+  @spec insert(map()) :: {:ok, struct()} | {:error, Ecto.ChangeError}
+  def insert(plant_traits_attrs) do
+    %PlantTraitsSchema{}
+    |> PlantTraitsSchema.changeset(plant_traits_attrs)
+    |> Repo.insert()
+    |> case do
+      {:ok, plant_trait} ->
+        {:ok, plant_trait}
+
+      {:error, %Ecto.Changeset{changes: changes, errors: error}} ->
+        Logger.error("## PARAMS")
+        Logger.info(plant_traits_attrs)
+        Logger.error("## INFORMATION TO INSERT:")
+        Logger.info(changes)
+        Logger.error("## ERROR DETAIL:")
+        Logger.info(error)
+        File.write("errors.text", changes)
+        error
+    end
+  end
+
+  @doc """
+
+  """
+  @spec insert_all(list()) :: {number(), list()} | nil
+  def insert_all(plant_traits_attrs) do
+    IO.inspect(length(plant_traits_attrs))
+
+    PlantTraitsSchema
+    |> Repo.insert_all(plant_traits_attrs)
+    |> IO.inspect()
+  end
+end
