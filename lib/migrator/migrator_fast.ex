1899db5d0d6a17b3586502104a993c0a39b8e27a
FTITOR <pac.ko1992@hotmail.com>
2022-05-15 13:14:15 -0500

    migration using genserver

--- lib/migrator/migrator_fast.ex
+++ lib/migrator/migrator_fast.ex
@@ -2,9 +2,8 @@ defmodule Chirinola.MigratorFast do
   @moduledoc """
   Documentation for `MigratorFast` module.
   """
-
   require Logger
-  alias Chirinola.PlantTrait
+  alias Chirinola.{QueueManager, Worker}
 
   @typedoc """
   Encoding modes, a tuple of two atoms.
@@ -42,6 +41,7 @@ defmodule Chirinola.MigratorFast do
   @wrong_path_message "Wrong path! Enter the absolute path of the file to migrate"
   @invalid_encoding_mode "The decoding format is invalid, check the valid formats"
 
+  @n_processes 1..200
   @file_not_found_message "File not found, enter the absolute path of the file to migrate"
   @headers_line "LastName\tFirstName\tDatasetID\tDataset\tSpeciesName\tAccSpeciesID\tAccSpeciesName\tObservationID\tObsDataID\tTraitID\tTraitName\tDataID\tDataName\tOriglName\tOrigValueStr\tOrigUnitStr\tValueKindName\tOrigUncertaintyStr\tUncertaintyName\tReplicates\tStdValue\tUnitName\tRelUncertaintyPercent\tOrigObsDataID\tErrorRisk\tReference\tComment\t\n"
 
@@ -74,19 +74,23 @@ defmodule Chirinola.MigratorFast do
 
   """
 
+  # path = "/home/mono/Documentos/try/17728_27112021022449/17728.txt"
+  # path = "/Users/ftitor/Downloads/17728_27112021022449/17728.txt"
+  # path = "/Users/ftitor/Downloads/17728_27112021022449/test.txt"
+
   @spec start(String.t(), encoding_mode()) :: atom()
   def start(path, encoding_mode \\ @default_encoding)
   def start(nil, _encoding_mode), do: Logger.error(@wrong_path_message)
   def start("", _encoding_mode), do: Logger.error(@wrong_path_message)
-
-  # path = "/Users/ftitor/Downloads/17728_27112021022449/17728.txt"
-  # path = "/Users/ftitor/Downloads/17728_27112021022449/test.txt"
 
   def start(_path, encoding_mode) when encoding_mode not in @valid_encondigs,
     do: Logger.error(@invalid_encoding_mode)
 
   def start(path, encoding_mode) do
     Logger.info("** MIGRATION PROCESS STARTED!")
+
+    {:ok, queue_pid} = QueueManager.start_link()
+    setup_process(queue_pid)
 
     path
     |> File.exists?()
@@ -96,12 +100,11 @@ defmodule Chirinola.MigratorFast do
 
         path
         |> File.stream!([encoding_mode], :line)
-        |> Stream.map(&process_line/1)
-        |> Enum.to_list()
-        |> Enum.reject(&(map_size(&1) == 0))
-        |> insert_all()
+        |> Stream.map(&migrate_line(&1, queue_pid))
+        |> Stream.run()
 
         Logger.info("** MIGRATION FINISHED!")
+        :ok
 
       false ->
         Logger.error(@file_not_found_message)
@@ -109,88 +112,87 @@ defmodule Chirinola.MigratorFast do
     end
   end
 
-  # process a line from the file
-  defp process_line(line) when line == @headers_line, do: %{}
-  defp process_line("\n"), do: %{}
+  defp setup_process(queue_pid) do
+    @n_processes
+    |> Enum.to_list()
+    |> Enum.each(fn _ ->
+      {:ok, worker_pid} = Worker.start_link()
+      QueueManager.push(queue_pid, worker_pid)
+    end)
+  end
 
-  defp process_line(line) do
+  defp migrate_line(line, _) when line == @headers_line,
+    do: Logger.info("HEADERS REMOVED!")
+
+  defp migrate_line("\n", _), do: Logger.info("EMPTY LINE")
+
+  defp migrate_line(line, queue_pid) do
     line
     |> String.split("\n")
     |> Enum.at(0)
     |> String.split("\t")
-    |> create_map()
+    |> create_struct()
+    |> insert_plant(queue_pid)
   end
 
-  defp create_map(plant) when length(plant) > 0 do
-    datetime =
-      NaiveDateTime.utc_now()
-      |> NaiveDateTime.truncate(:second)
+  defp create_struct([]), do: %{}
 
+  defp create_struct(plant) do
     Map.new()
-    |> Map.put(:last_name, Enum.at(plant, 0))
-    |> Map.put(:first_name, Enum.at(plant, 1))
-    |> Map.put(:dataset_id, validate_integer(Enum.at(plant, 2)))
-    |> Map.put(:dataset, Enum.at(plant, 3))
-    |> Map.put(:species_name, Enum.at(plant, 4))
-    |> Map.put(:acc_species_id, validate_integer(Enum.at(plant, 5)))
-    |> Map.put(:acc_species_name, Enum.at(plant, 6))
-    |> Map.put(:observation_id, validate_integer(Enum.at(plant, 7)))
-    |> Map.put(:obs_data_id, validate_integer(Enum.at(plant, 8)))
-    |> Map.put(:trait_id, validate_integer(Enum.at(plant, 9)))
-    |> Map.put(:trait_name, Enum.at(plant, 10))
-    |> Map.put(:data_id, validate_integer(Enum.at(plant, 11)))
-    |> Map.put(:data_name, Enum.at(plant, 12))
-    |> Map.put(:origl_name, Enum.at(plant, 13))
-    |> Map.put(:orig_value_str, Enum.at(plant, 14))
-    |> Map.put(:orig_unit_str, Enum.at(plant, 15))
-    |> Map.put(:value_kind_name, Enum.at(plant, 16))
-    |> Map.put(:orig_uncertainty_str, Enum.at(plant, 17))
-    |> Map.put(:uncertainty_name, Enum.at(plant, 18))
-    |> Map.put(:replicates, validate_float(Enum.at(plant, 19), :replicates))
-    |> Map.put(:std_value, validate_float(Enum.at(plant, 20), :std_value))
-    |> Map.put(:unit_name, Enum.at(plant, 21))
-    |> Map.put(
-      :rel_uncertainty_percent,
-      validate_float(Enum.at(plant, 22), :rel_uncertainty_percent)
-    )
-    |> Map.put(:orig_obs_data_id, validate_integer(Enum.at(plant, 23)))
-    |> Map.put(:error_risk, validate_float(Enum.at(plant, 24), :error_risk))
-    |> Map.put(:reference, Enum.at(plant, 25))
-    |> Map.put(:comment, Enum.at(plant, 26))
-    |> Map.put(:no_name_column, Enum.at(plant, 27))
-    |> Map.put(:inserted_at, datetime)
-    |> Map.put(:updated_at, datetime)
+    |> Map.put("last_name", Enum.at(plant, 0))
+    |> Map.put("first_name", Enum.at(plant, 1))
+    |> Map.put("dataset_id", Enum.at(plant, 2))
+    |> Map.put("dataset", Enum.at(plant, 3))
+    |> Map.put("species_name", Enum.at(plant, 4))
+    |> Map.put("acc_species_id", Enum.at(plant, 5))
+    |> Map.put("acc_species_name", Enum.at(plant, 6))
+    |> Map.put("observation_id", Enum.at(plant, 7))
+    |> Map.put("obs_data_id", Enum.at(plant, 8))
+    |> Map.put("trait_id", Enum.at(plant, 9))
+    |> Map.put("trait_name", Enum.at(plant, 10))
+    |> Map.put("data_id", Enum.at(plant, 11))
+    |> Map.put("data_name", Enum.at(plant, 12))
+    |> Map.put("origl_name", Enum.at(plant, 13))
+    |> Map.put("orig_value_str", Enum.at(plant, 14))
+    |> Map.put("orig_unit_str", Enum.at(plant, 15))
+    |> Map.put("value_kind_name", Enum.at(plant, 16))
+    |> Map.put("orig_uncertainty_str", Enum.at(plant, 17))
+    |> Map.put("uncertainty_name", Enum.at(plant, 18))
+    |> Map.put("replicates", validate_replicate(Enum.at(plant, 19)))
+    |> Map.put("std_value", Enum.at(plant, 20))
+    |> Map.put("unit_name", Enum.at(plant, 21))
+    |> Map.put("rel_uncertainty_percent", Enum.at(plant, 22))
+    |> Map.put("orig_obs_data_id", Enum.at(plant, 23))
+    |> Map.put("error_risk", Enum.at(plant, 24))
+    |> Map.put("reference", Enum.at(plant, 25))
+    |> Map.put("comment", Enum.at(plant, 26))
+    |> Map.put("no_name_column", Enum.at(plant, 27))
   end
 
-  defp create_map(_), do: %{}
+  defp insert_plant(plant, _) when map_size(plant) == 0, do: Logger.info("- EMPTY LINE")
 
-  defp validate_integer(nil), do: nil
-  defp validate_integer(""), do: nil
-  defp validate_integer(number) when is_integer(number), do: number
-  defp validate_integer(number) when is_binary(number), do: String.to_integer(number)
+  defp insert_plant(plant, queue_pid) do
+    {:ok, worker_pid} = QueueManager.get_pid(queue_pid)
 
-  defp validate_float(nil, _field), do: nil
-  defp validate_float("", _field), do: nil
-  defp validate_float(number, _field) when is_float(number), do: number
+    Worker.insert(worker_pid, plant)
+  end
 
-  defp validate_float(number, field) when is_binary(number) do
-    number
+  defp validate_replicate(nil), do: nil
+  defp validate_replicate(""), do: nil
+  defp validate_replicate(replicate) when is_float(replicate), do: replicate
+
+  defp validate_replicate(replicate) when is_binary(replicate) do
+    replicate
     |> Float.parse()
     |> case do
       {float, _} ->
         float
 
       :error ->
-        Logger.error("¡Error! Field: #{field}; Value: #{number}")
+        Logger.error("Replicate invalid: #{replicate}")
         nil
     end
   end
 
-  defp validate_float(_, _), do: nil
-
-  defp insert_all(list) do
-    list
-    |> Enum.chunk_every(2_000)
-    |> Enum.each(&PlantTrait.insert_all/1)
-  end
+  defp validate_replicate(_), do: nil
 end
