604e9f96075640c179ef6be91d258be81596e17f
FTITOR <pac.ko1992@hotmail.com>
2022-06-03 22:42:08 -0500

    adjust Chirinola.Worker

--- lib/worker.ex
+++ lib/worker.ex
@@ -9,28 +9,15 @@ defmodule Chirinola.Worker do
 
   def start_link(), do: GenServer.start_link(__MODULE__, [])
 
-  def insert(pid, element), do: GenServer.cast(pid, {:insert, element})
-
-  def insert_all(pid, element), do: GenServer.cast(pid, {:insert_all, element})
+  def insert(pid, element, file_name), do: GenServer.cast(pid, {:insert, element, file_name})
 
   @impl true
   def init(stack), do: {:ok, stack}
 
   @impl true
-  def handle_cast({:insert, element}, state) do
-    PlantTrait.insert(element)
+  def handle_cast({:insert, element, file_name}, state) do
+    PlantTrait.migrate_line(element, file_name)
 
     {:noreply, state}
-  end
-
-  @impl true
-  def handle_cast({:insert_all, element}, state) when length(state) < 200 do
-    {:noreply, state ++ [element]}
-  end
-
-  def handle_cast({:insert_all, element}, state) when length(state) == 200 do
-    PlantTrait.insert_all(state ++ [element])
-
-    {:noreply, []}
   end
 end
